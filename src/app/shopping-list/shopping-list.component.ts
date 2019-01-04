41553c5734d2b85bed6bad8ed106c7e3016288f1
yolixtly <yolixtly_13@hotmail.com>
2019-01-03 19:19:21 -0500

    Static content for shopping list component

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Ingredient } from '../shared/ingredient.model';
 @Component({
     selector: 'app-shopping-list',
     templateUrl: './shopping-list.component.html',
     styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+    ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Bananas', 2),
+    ];
     constructor() { }
 
     ngOnInit() {
