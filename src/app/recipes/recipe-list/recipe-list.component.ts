b1688f410319fd600ca4e7b5dbf89b973adda8b2
yolixtly <yolixtly_13@hotmail.com>
2019-01-03 19:08:09 -0500

    Basic Contents for recipe List

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
     selector: 'app-recipe-list',
     templateUrl: './recipe-list.component.html',
     styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+    recipes: Recipe[] = [
+        new Recipe('Enchiladas', 'Mexican dish',
+        'https://www.jessicagavin.com/wp-content/uploads/2018/04/chicken-enchiladas-5-1200.jpg'),
+    ];
 
     constructor() { }
 
